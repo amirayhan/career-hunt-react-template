1bdf830d468289c662d994203ac12b87f80e7d2c
amirayhan <jaber.ac1983@gmail.com>
2023-09-17 15:44:16 +0600

    header is actived

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -6,30 +6,42 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+    const [fix, setfix] = useState(false);
+
+    const setFixed = () => {
+        if (window.scrollY >= 140) {
+            setfix(true);
+        } else {
+            setfix(false);
+        }
+    };
+
+    window.addEventListener("scroll", setFixed);
+
     return (
-        <Navbar expand="lg">
+        <div className={`${fix ? "navbar fixed" : "navbar"} navbar-expand-lg navbar-light`} expand="lg">
             <Container>
                 <Navbar.Brand href="#home">CareerHunt</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="m-auto">
                         <li className="nav-item">
-                            <Link to="/" className="nav-link">
+                            <Link to="/" className="nav-link" exact activeClassName="active">
                                 Home
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/statistics" className="nav-link">
+                            <Link to="/statistics" className="nav-link" activeClassName="active">
                                 Statistics
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/appliedjobs" className="nav-link">
+                            <Link to="/appliedjobs" className="nav-link" activeClassName="active">
                                 Applied Jobs
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/blog" className="nav-link">
+                            <Link to="/blog" className="nav-link" activeClassName="active">
                                 Blog
                             </Link>
                         </li>
@@ -37,7 +49,7 @@ const Header = () => {
                     <button className="button1">Start Applying</button>
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </div>
     );
 };
 
