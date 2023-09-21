b522832391b57109939adf332c3ec46f42e6830c
rayhan <jaber.ac1983@gmail.com>
2023-09-21 07:53:33 +0600

    sticky header

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,19 +7,16 @@ const Banner = () => {
         <section id="banner">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-6">
+                    <div className="col-lg-12">
                         <div className="banner_content">
                             <h1>
                                 One Step <br /> Closer To Your <br />
                                 <span className="color_p">Dream Job</span>
                             </h1>
-                            <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                            <p>
+                                Explore thousands of job opportunities with all the information you need. Its your future. Come find it. <br /> Manage all your job application from start to finish.
+                            </p>
                             <button className="button1">Get Started</button>
-                        </div>
-                    </div>
-                    <div className="d-none d-sm-none d-md-none col-lg-6 ">
-                        <div className="banner_image">
-                            <img src={heroImage} alt="Hero image" />
                         </div>
                     </div>
                 </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,25 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+    // const [fix, setfix] = useState(false);
+
+    // const setFixed = () => {
+    //     if (window.scrollY >= 140) {
+    //         setfix(true);
+    //     } else {
+    //         setfix(false);
+    //     }
+    // };
+
+    // window.addEventListener("scroll", setFixed);
+
+    const [toggle, setToggle] = useState(false);
     const [fix, setfix] = useState(false);
 
     const setFixed = () => {
@@ -18,40 +33,113 @@ const Header = () => {
 
     window.addEventListener("scroll", setFixed);
 
+    const handleToggleClick = () => setToggle(!toggle);
+
     return (
-        <div className={`${fix ? "navbar fixed" : "navbar"} navbar-expand-lg navbar-light`} expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">CareerHunt</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="m-auto">
-                        <li className="nav-item">
+        <>
+            <div className={fix ? "header fixed" : "header"}>
+                <div className="container d-flex justify-content-between align-items-center py-3">
+                    <div className="logo">
+                        <a href="/">
+                            <h3>CareerHunt</h3>
+                        </a>
+                    </div>
+                    <div className="d-none d-md-flex align-items-center gap-5">
+                        <ul className="d-md-flex gap-5">
+                            <li>
+                                <Link to="/" className="nav-link">
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/statistics" className="nav-link">
+                                    Statistics
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/appliedjobs" className="nav-link">
+                                    Applied Jobs
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/blog" className="nav-link">
+                                    Blog
+                                </Link>
+                            </li>
+                        </ul>
+                        <a href="#featured_jobs" className="button1">
+                            Start Applying
+                        </a>
+                    </div>
+                    <button onClick={handleToggleClick} className="d-md-none">
+                        <FontAwesomeIcon icon={toggle ? faXmark : faBars} />
+                    </button>
+                </div>
+                <div className={`${toggle ? "position-absolute" : "d-none"} w-100 m-auto toggle-navbar`}>
+                    <ul className="text-center">
+                        <li className="mb-3">
                             <Link to="/" className="nav-link">
                                 Home
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="mb-3">
                             <Link to="/statistics" className="nav-link">
                                 Statistics
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="mb-3">
                             <Link to="/appliedjobs" className="nav-link">
                                 Applied Jobs
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="mb-3">
                             <Link to="/blog" className="nav-link">
                                 Blog
                             </Link>
                         </li>
-                    </Nav>
-                    <a href="#featured_jobs" className="button1">
-                        Start Applying
-                    </a>
-                </Navbar.Collapse>
-            </Container>
-        </div>
+                    </ul>
+                    <div className="d-flex justify-content-center">
+                        <a href="#featured_jobs" className="button1 m-auto">
+                            Start Applying
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className={`${fix ? "navbar fixed" : "navbar"} navbar-light`} expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">CareerHunt</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="m-auto">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/statistics" className="nav-link">
+                                    Statistics
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/appliedjobs" className="nav-link">
+                                    Applied Jobs
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/blog" className="nav-link">
+                                    Blog
+                                </Link>
+                            </li>
+                        </Nav>
+                        <a href="#featured_jobs" className="button1">
+                            Start Applying
+                        </a>
+                    </Navbar.Collapse>
+                </Container>
+            </div> */}
+        </>
     );
 };
 
